fad4fca72deaccc6cd9b81d0780f2a7273f433e9
Todd <appalachee.red@gmail.com>
2018-08-21 16:28:44 -0400

    passed tests

--- step_4.py
+++ step_4.py
@@ -1,6 +1,21 @@
 from step_1 import transform_products_to_list
-
+from pprint import pprint
 
 
 def calculate_total_per_invoices(products_string):
-    pass
+    products_list = transform_products_to_list(products_string)
+    product = {}
+    for products in products_list:
+        invoice = products[0]
+        customer_id = products[-2]
+        unit_price = float(products[5])
+        quantity = float(products[3])
+
+        total = unit_price * quantity
+
+        product.setdefault(customer_id, {})
+        product[customer_id].setdefault(invoice, 0)
+        product[customer_id][invoice] += total
+        product[customer_id][invoice] = round(product[customer_id][invoice], 2)
+
+    return product
