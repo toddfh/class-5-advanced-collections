e654e6a80101bdac903621dfb9604af569918972
Todd <appalachee.red@gmail.com>
2018-08-21 14:22:11 -0400

    tests working

--- step_1.py
+++ step_1.py
@@ -1,2 +1,15 @@
+from data import products_string
+
 def transform_products_to_list(products_string):
-    pass
+    split_products = products_string.split('\n')
+    products_list = []
+
+    for product_line in split_products:
+        if product_line:
+            product = product_line.split(',')
+            products_list.append(product)
+
+    #print(products_list)
+    return products_list
+
+transform_products_to_list(products_string)
--- step_3.py
+++ step_3.py
@@ -1,5 +1,39 @@
+from data import products_string
+from pprint import pprint
 from step_1 import transform_products_to_list
 
 
 def group_products_by_customer_and_invoice(products_string):
-    pass
+    products_list = transform_products_to_list(products_string)
+
+    products = {}
+
+    for product in products_list:
+        invoice = product[0]
+        customer_id = product[-2]
+
+        products.setdefault(customer_id, {})
+
+        products[customer_id].setdefault(invoice, [])
+        products[customer_id][invoice].append(product)
+
+    return products
+
+
+
+
+products = group_products_by_customer_and_invoice(products_string)
+
+customer = products['12583']
+# print(customer)
+invoice = customer['536370']
+# print(invoice)
+item = invoice[1]
+# print(item)
+weird_description = item[2]
+# print(weird_description)
+
+alternate_way = products['12583']['536370'][1][2]
+print(alternate_way)
+
+pprint(products)
